b5467248f748aa0ca92ba49e83d10f8abc9d90f8
Techakkord <jonakkord@mail.ru>
2023-03-04 02:44:31 +0600

    hw1 task2 complete

--- hw1/Task2/Program.cs
+++ hw1/Task2/Program.cs
@@ -1,2 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿//Евгений Филимонов 
+//Знакомство с языками программирования (семинары)
+//ДЗ 1. Задача 2.
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int firstNum = Convert.ToInt32(Console.ReadLine());
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+if (firstNum > secondNum) Console.WriteLine($"Число {firstNum} больше числа {secondNum}"); 
+else if (secondNum > firstNum) Console.WriteLine($"Число {secondNum} больше числа {firstNum}");
+else if (firstNum == secondNum) Console.WriteLine("Введённые числа равны");
+else Console.WriteLine("Не удалось сравнить значения");
