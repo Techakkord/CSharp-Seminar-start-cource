0252cbe7c8342732ce91cd8135a7f4f3fff5d06a
Techakkord <jonakkord@mail.ru>
2023-03-04 03:26:19 +0600

    hw1 task6 complete

---
+++ hw1/Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Евгений Филимонов 
+//Знакомство с языками программирования (семинары)
+//ДЗ 1. Задача 4.
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int firstNum = Convert.ToInt32(Console.ReadLine());
+int secondNum = Convert.ToInt32(Console.ReadLine());
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+int maxNum = firstNum;
+
+if (secondNum > maxNum) maxNum = secondNum;
+if (thirdNum > maxNum) maxNum = thirdNum;
+Console.WriteLine($"Максимальное число из трёх {maxNum}");
