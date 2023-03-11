f00961f411c04ff70d180532f81f2a565db805cd
Techakkord <jonakkord@mail.ru>
2023-03-11 09:25:02 +0600

    hw3 Complete task19

---
+++ hw3/Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿void isPalindrome (int num){
+    int reverse = 0;
+    int temp = num;
+    while (temp != 0) {
+        reverse = (reverse * 10) + (temp % 10);
+        temp = temp / 10;
+    }
+    if (reverse == num) Console.Write("Number is palindrome");
+    else Console.Write("Number is not palindrome");
+}
+
+/*bool isPalindrome (int num){
+    int reverse = 0;
+    int temp = num;
+    while (temp != 0) {
+        reverse = (reverse * 10) + (temp % 10);
+        temp = temp / 10;
+    }
+    return (reverse == num);
+}*/
+
+int number = Convert.ToInt32(Console.ReadLine());
+isPalindrome(number);
