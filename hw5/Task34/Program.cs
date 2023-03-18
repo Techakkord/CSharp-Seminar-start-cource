a9631d1587a9582ca9bc80d1d971b707196bda7f
Techakkord <jonakkord@mail.ru>
2023-03-18 11:27:18 +0600

    hw5 Task5 Complete

---
+++ hw5/Task34/Program.cs
@@ -0,0 +1,29 @@
+﻿//Евгений Филимонов 
+//Знакомство с языками программирования (семинары)
+//ДЗ 5. Задача 34.
+/*Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет 
+количество чётных чисел в массиве*/
+// [345, 897, 568, 234] -> 2
+int[] arr = getRandArr(4);
+Console.WriteLine($"=> {getEvenCount(arr)}");
+
+int[] getRandArr(int quantity){
+    int[] arr = new int[quantity];
+    for (int i = 0; i < quantity; ++i)
+    {
+        Random rand = new Random();
+        arr[i] = rand.Next(100,999);
+        Console.Write($"{arr[i]} ");
+    }
+    return arr;
+}
+
+int getEvenCount(int[] arr){
+    int count = 0;
+    for (int i = 0; i < arr.Length; ++i)
+    {
+        if(arr[i]%2 == 0) ++count;
+    }
+    return count;
+}    
