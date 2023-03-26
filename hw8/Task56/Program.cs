83e8a32b9ab7b6f731beb546160eeed82cf7c8a6
Techakkord <jonakkord@mail.ru>
2023-03-26 12:42:21 +0600

    hw8 task56 done

--- hw8/Task56/Program.cs
+++ hw8/Task56/Program.cs
@@ -1,26 +1,42 @@
 ﻿//Знакомство с языками программирования (семинары)
-//ДЗ 8. Задача 54
-/*Задайте двумерный массив. 
-Напишите программу, которая упорядочит по убыванию элементы каждой строки 
-двумерного массива.*/
-
+//ДЗ 8. Задача 56
+/*Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.*/
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+// с наименьшей суммой элементов: 1 строка
 
-int m = 3;
-int n = 4;
-int[,]arr = MyArr.getArr(m,n);
+int[,] arr = new int[4,4]{{1,4,7,2},{5,9,2,3},{8,4,2,4},{5,2,6,7}};
 MyArr.displayArr(arr);
-int[,] sortArr = MyArr.getDescendingSortArr(arr);
-MyArr.displayArr(sortArr);
+MyArr.getLessRowBySum(arr);
+
 public static class MyArr{
-  
+
+    public static int getLessRowBySum(int[,] arr){
+        int lessRow = 0;
+        int sum = 0;
+        int minSum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum = 0;
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                sum+=arr[i,j];
+            }
+            if(i == 0) minSum = sum;
+            else if (sum < minSum){
+                minSum = sum;
+                lessRow = i;
+            } 
+        }
+        Console.WriteLine($"{lessRow+1}-я строка с наименьшей суммой элементов"); 
+        return lessRow;
+        
+    }  
     public static int[,] getAscendingSortArr(int[,] arr){
         for (int i = 0; i < arr.GetLength(0); i++)
         {
@@ -51,7 +67,7 @@ public static class MyArr{
                 {
                     if(arr[i,j] < arr[i,minId]) minId = j;
                 }
-                temp = arr[i,size -1];56
+                temp = arr[i,size -1];
                 arr[i,size -1] = arr[i,minId];
                 arr[i,minId] =temp;
                 --size;
