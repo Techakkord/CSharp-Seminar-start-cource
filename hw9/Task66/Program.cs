2de3950da798ce0504a756a12b70dd69319b245e
Techakkord <jonakkord@mail.ru>
2023-04-01 00:52:57 +0600

    hw9 Task66 done

---
+++ hw9/Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿//Знакомство с языками программирования (семинары)
+//ДЗ 9. Задача 66
+/*Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.*/
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int M = 1;
+int N = 15;
+Console.WriteLine(getSum(M,N));
+
+M =4;
+N =8;
+Console.WriteLine(getSum(M,N));
+
+int getSum(int start, int end){
+    if(start == end) return end;
+    else if(start<end) 
+        return  start + getSum(start+1, end); 
+    else return start + getSum(start -1, end);
+}
